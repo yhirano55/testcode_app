5d8489a4b576ef0c62ea011bfc2a354a6318ff7a
Yoshiyuki Hirano <yhirano@me.com>
2019-05-16 06:43:13 +0900

    Impl User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, email_format: true
+
+  def ban!
+    update!(banned: true)
+  end
+
+  def unban!
+    update!(banned: false)
+  end
+
+  def access_token
+    verifier.generate({ id: id, email: email }, purpose: :login, expires_in: 24.hours)
+  end
+
+  private
+
+  def verifier
+    Rails.application.message_verifier('testcode_app')
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#create" do
+    user = User.create(email: 'admin@example.com', password: 'password')
+    assert user.persisted?
+  end
+
+  test "#ban!" do
+    user = User.create(email: 'admin@example.com', password: 'password', banned: false)
+    assert !user.banned?
+
+    user.ban!
+    assert user.banned?
+  end
+
+  test "#unban!" do
+    user = User.create(email: 'admin@example.com', password: 'password', banned: true)
+    assert user.banned?
+
+    user.unban!
+    assert !user.banned?
+  end
+
+  test "#access_token" do
+    user = User.create(email: 'admin@example.com', password: 'password')
+    assert_kind_of String, user.access_token
+  end
 end
