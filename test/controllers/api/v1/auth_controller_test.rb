d629fe7a256c7648ff1271b707c51e93bee5b9f6
Hang Gao <wohangye@gmail.com>
2019-05-16 12:55:23 +0900

    update

--- test/controllers/api/v1/auth_controller_test.rb
+++ test/controllers/api/v1/auth_controller_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class Api::V1::AuthControllerTest < ActionDispatch::IntegrationTest
 
   test "returns 200 status, if user could authenticate" do
-    skip 'implement this'
+    User.create(email: "admin@example.com", password: "password")
+    post "/api/v1/auth", params: { email: "admin@example.com", password: "password"}
+
+    assert_equal 200, @response.status
+    parsed_json = JSON.parse(@response.body)
+    assert parsed_json.has_key?("access_token")
+    # assert_kind_of String,
   end
 
   test "returns 401 status, if user could not authenticate" do
@@ -15,11 +21,24 @@ class Api::V1::AuthControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "returns 403 status, if user is banned" do
-    skip 'implement this'
+
+    User.create(email: "admin@example.com", password: "password", banned: true)
+    post "/api/v1/auth", params: { email: "admin@example.com", password: "password"}
+    
+    assert_equal 403, @response.status
+    # parsed_json = JSON.parse(@response.body)
+    # assert parsed_json.has_key?("access_token")
   end
 
   test "returns 401 status, if email or password is not given" do
-    skip 'implement this'
+    post "/api/v1/auth"
+
+    # byebug
+
+    assert_equal 401, @response.status
+    parsed_json = JSON.parse(@response.body)
+    assert_equal 401, parsed_json["error"]["code"]
+ 
   end
 
 end
